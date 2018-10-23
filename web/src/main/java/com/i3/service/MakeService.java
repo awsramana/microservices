0247f029a66a71a7b63b1dc1fb99dea24b2f4dce
awsramana <aws.ramana@gmail.com>
2018-10-23 15:37:42 -0400

    added docker kubernetes

--- web/src/main/java/com/i3/service/MakeService.java
+++ web/src/main/java/com/i3/service/MakeService.java
@@ -8,7 +8,8 @@ public class MakeService {
 	
 	
 	public String message(String userName) {
-		String SERVICE_URL="http://localhost:9081/message/{userName}";
+		String SERVICE_URL="http://192.168.64.8:30024/message/{userName}";
+		System.out.println("Gateway URL::"+SERVICE_URL);
 		return new RestTemplate().getForObject(SERVICE_URL, String.class, userName);
 	}
 	
